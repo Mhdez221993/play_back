6c785b131cffd3996fac336de0ccdfc2094a859b
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-24 18:43:19 +0100

    Wrap login with react-bootstrap container

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,12 +1,13 @@
+import { Container } from 'react-bootstrap';
 import React from 'react';
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=897d1580943f4bbba28040d5757aecbe&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
 
 const Login = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100">
+    <Container className="d-flex justify-content-center align-items-center vh-100">
       <a className="btn btn-success btn-lg" href={AUTH_URL}>Login With Spotify</a>
-    </div>
+    </Container>
   );
 };
 
