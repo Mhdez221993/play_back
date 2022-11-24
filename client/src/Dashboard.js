eca1903c5ca12eed655bfc83e8f9aa704e7a7618
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-24 22:48:34 +0100

    Crease search input

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import { Container, Form } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+
 import auth from './useAuth';
 
 const Dashboard = ({code}) => {
   const accessToken = auth(code)
 
+  const [search, setSearch] = useState('')
+  const [searchResult, setSearchResult] = useState('')
+
+  useEffect(() =>{
+    if (search === '') return
+    if (!accessToken) return setSearchResult([])
+
+
+
+  }, [search, accessToken])
+
   return (
-    <div>
-      <h1>{code}</h1>
-    </div>
+    <Container>
+      <Form.Control
+        type="search"
+        placeholder="Saerch Songs/Artis..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+
+    </Container>
   );
 };
 
