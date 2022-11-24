17876b0868993d92426efb02c6eb08ac3149201d
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-24 22:06:49 +0100

    Get access token

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -4,18 +4,21 @@ import axios from 'axios';
 
 export default function useAuth(code) {
   const [accessToken, setAccessToken] = useState()
+  const [expiresIin, setExpiresIin] = useState()
+  const [refreshToken, setRefreshToken] = useState()
 
   useEffect(() => {
     axios
       .post('http://localhost:3001/login', {code})
       .then(res => {
-        setAccessToken(res.body.access_token)
-        console.log(res.body);
+        setAccessToken(res.data.access_token)
+        setExpiresIin(res.data.expires_in)
+        setRefreshToken(res.data.refresh_token)
       })
       .catch(err => {
         window.location = '/'
       })
-  }, [code])
+  }, [code, accessToken])
 
   return accessToken
 };
