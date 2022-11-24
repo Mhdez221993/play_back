e1d45c1340aada7f76ffd855bad7a266f758507b
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-24 22:48:50 +0100

    Update server

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
-require('dotenv').config();
+require('dotenv').config()
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const bodyParser = require('body-parser');
+const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
-
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refreshToken
+
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URL,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken,
+  })
+
+  spotifyApi.refreshAccessToken()
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        expiresIn: data.body.expires_in,
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      res.sendStatus(400)
+    })
+})
 
 app.post('/login', (req, res) => {
-  const code = req.body.code
+  const code = req.body.code;
 
   const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URL,
@@ -19,18 +43,27 @@ app.post('/login', (req, res) => {
   })
 
   spotifyApi.authorizationCodeGrant(code)
-    .then((data) => {
-      const {access_token, expires_in, refresh_token} = data.body
+   .then((data) => {
 
-      res.json({
-         access_token,
-         expires_in,
-         refresh_token,
-      })
+    res.json({
+      accessToken: data.body.access_token,
+      refreshToken: data.body.refresh_token,
+      expiresIn: data.body.expires_in,
     })
-    .catch(err => {
-      console.log(err);
-    })
+   })
+   .catch((err) => {
+    console.log(err);
+    res.sendStatus(400)
+   })
 })
 
-app.listen(process.env.PORT, console.log(`app listening on port ${process.env.PORT}`));
+app.get('/lyrics', async (req, res) => {
+  const lyrics = await lyricsFinder(req.query.artist, req.query.title) || "Not Found!";
+  console.log(lyrics);
+
+  res.json({ lyrics })
+})
+
+app.listen(3001, () => {
+  console.log('app listening on port 3001');
+});
