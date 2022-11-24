e2fc9ef177d9818dcb575fce3a6e9a99c740fcb5
Moises Hernandez <mhdezcoronado@gmail.com>
2022-11-24 18:44:09 +0100

    Grap the code when authentication is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Dashboard from './Dashboard';
 import Login from "./Login";
 import React from "react";
 
+const code = new URLSearchParams(window.location.search).get('code');
+
 function App() {
-  return <Login />
+  return !code  ? <Login /> : <Dashboard code={code} />;
 }
 
 export default App;
